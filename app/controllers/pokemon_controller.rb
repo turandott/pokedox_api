f74e1d5ccd995e9af774f640c02825f575d8c19c
turandott <greyvilka@gmail.com>
2022-09-01 14:32:44 +0300

    Initial commit with Faraday gem

---
+++ app/controllers/pokemon_controller.rb
@@ -0,0 +1,34 @@
+require 'json'
+class PokemonController < ApplicationController
+
+  def index
+    pokemons=[]
+    url='https://pokeapi.co/api/v2/pokemon'
+    response=Faraday.get(url,max_response: 10)
+     @row_data=JSON.parse(response.body)
+    @row_data.each do |key, value|
+      if key=="results"
+        value.each do |k, v|
+          response=Faraday.get(k["url"])
+          response=JSON.parse(response.body)
+          pokemons<<response
+        end
+      end
+    end
+    @pokemons=@row_data['results'].map{ |n| n['name']}
+
+    # @pokemon_json=pokemons.map do |pokemon|
+      # Pokemon.new(img:pokemon['sprites']['other']['dream_world']["front_default"], name: pokemon['forms'][0]['name'], weight: pokemon['weight'], poke_type: pokemon['types'][0]['type']['name'], poke_ability: pokemon['abilities'][0]['ability']['name'], poke_id: pokemon['id'])
+    end
+    # render json: { pokemons: @pokemon_json }
+  # end
+
+    # pokemon=gets.strip.upcase
+    #
+    # found=pokemons.select do |pokemon|
+    #   pokemon.upcase.include?(pokemon)
+    # end
+    # puts found.first
+
+
+end
---
+++ db/migrate/20220901082541_create_pokemons.rb
@@ -0,0 +1,9 @@
+class CreatePokemons < ActiveRecord::Migration[6.0]
+  def change
+    create_table :pokemons do |t|
+
+
+      t.timestamps
+    end
+  end
+end
