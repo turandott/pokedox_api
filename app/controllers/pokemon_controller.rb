4fc5cea6f4f79e19902852e782a1e58f7c64a4e5
turandott <greyvilka@gmail.com>
2022-09-03 16:18:02 +0300

    Add pokemon list view for index method

--- app/controllers/pokemon_controller.rb
+++ app/controllers/pokemon_controller.rb
@@ -1,23 +1,22 @@
 require 'json'
 class PokemonController < ApplicationController
   def index
+    pokemons_test=[]
     pokemons=[]
-    url='https://pokeapi.co/api/v2/pokemon'
-    response=Faraday.get(url,max_response: 10)
+    url='https://pokeapi.co/api/v2/pokemon?limit=5'
+    response=Faraday.get(url)
      @row_data=JSON.parse(response.body)
     @row_data.each do |key, value|
       if key=="results"
         value.each do |k, v|
           response=Faraday.get(k["url"])
-          response=JSON.parse(response.body)
-          pokemons<<response
+          @response=JSON.parse(response.body)
+          pokemons<<@response
         end
       end
     end
-    @pokemons=@row_data['results'].map{ |n| n['name']}
+    @pokemons=pokemons
 
-    # @pokemon_json=pokemons.map do |pokemon|
-      # Pokemon.new(img:pokemon['sprites']['other']['dream_world']["front_default"], name: pokemon['forms'][0]['name'], weight: pokemon['weight'], poke_type: pokemon['types'][0]['type']['name'], poke_ability: pokemon['abilities'][0]['ability']['name'], poke_id: pokemon['id'])
     end
     # render json: { pokemons: @pokemon_json }
   # end
