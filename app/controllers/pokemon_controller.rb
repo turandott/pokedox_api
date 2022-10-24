0e4ed08c6be5157633177a0a582c42b02de18d61
turandott <greyvilka@gmail.com>
2022-10-24 21:55:32 +0300

    update random pokemon action

--- app/controllers/pokemon_controller.rb
+++ app/controllers/pokemon_controller.rb
@@ -1,7 +1,5 @@
 require 'pokemon_request'
-# require 'pry-rails'
 class PokemonController < ApplicationController
-
   # skip_before_action :verify_authenticity_token
   def index
     # pokemons=Pokemon.all.delete_all
@@ -11,15 +9,6 @@ class PokemonController < ApplicationController
     @pokemon=Pokemon.create_pokemon(@pokemon)
     end
     @pokemons=Pokemon.order(:id).page(params[:page])
-    # pokemon=params[:pokemon]
-    # pokemon=PokemonRequest.get_pokemon(pokemon)
-    # # binding.pry
-    # if !pokemon.nil?
-    #   poke=Poke.new(pokemon)
-    #   @pokemon=Pokemon.create_pokemon(poke)
-    # else
-    #   redirect_to root_path
-    # end
   end
 
   def new
@@ -31,48 +20,17 @@ class PokemonController < ApplicationController
       else
         redirect_to root_path
       end
-  #   return if params[:search].blank?
-  #   raw_response = Faraday.get "https://pokeapi.co/api/v2/pokemon/#{params[:search]}"
-  #   if raw_response.status == 200
-  #     @response = JSON.parse(raw_response.body)
-  #     @pokemon=Pokemon.new(order: @response["id"], name: @response["name"], image_url: @response["sprites"]["front_default"], weight: @response['weight'],poke_type: @response['types'][0]['type']['name'], poke_ability: @response['abilities'][0]['ability']['name'])
-  #   else
-  #     flash.now[:error] = "No such pokemon!"
-  #     render action: "new"
-  #     # render status: 400
-  #     # redirect_to new_pokemon_path, notice: "#{raw_response.status}error!"
-  #   end
   end
   def show
     @pokemon = Pokemon.find(params[:id])
   end
   #
   def random_pokemon
-    @random_pokemon=Pokemon.limit(1).order("RANDOM()")
-
-
-    #   url='https://pokeapi.co/api/v2/pokemon-species/?limit=0'
-  #   response=Faraday.get(url)
-  #   @row_data=JSON.parse(response.body)
-  #   @row_data.each do |key, value|
-  #     if key=="count"
-  #       @number=value
-  #     end
-  #   end
-  #   @pokemon_number=rand(1...@number)
-  #   raw_response = Faraday.get "https://pokeapi.co/api/v2/pokemon/#{@pokemon_number}"
-  #   @response = JSON.parse(raw_response.body)
-  # end
-  # def create
-  #   @pokemon=Pokemon.new(pokemon_params)
-  #   if @pokemon.save
-  #     redirect_to pokemon_index_path
-  #     # render :json
-  #   else
-  #     render :new
-  #   end
-
-  #
+    @random_pokemon = Pokemon.find(rand(Pokemon.first.id..Pokemon.last.id))
+    # @random_pokemon = Pokemon.find(rand(1..Pokemon.count))
+    # @random_pokemon=Pokemon.all.shuffle.last
+    # @random_pokemon=Pokemon.find(Pokemon.pluck(:id).sample(1))
+    # @random_pokemon=Pokemon.limit(1).order("RANDOM()")
   end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root "pokemon#index"
   resources :pokemon, only: [:index, :new, :create, :show]
-  get '/pokemon/random_pokemon', to: 'pokemon#random_pokemon'
+  get '/pokemon/random_pokemon/:id', to: 'pokemon#random_pokemon',:action=>"show"
+
 end
